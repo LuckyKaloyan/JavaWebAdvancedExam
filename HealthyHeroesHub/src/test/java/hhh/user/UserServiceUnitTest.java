1f0d6b0e87a12c3d239028626959082a10f93ec8
Kaloyan Dimitrov <luckykaloyan@gmail.com>
2025-04-01 00:51:54 +0300

    EndGameSADGE

--- HealthyHeroesHub/src/test/java/hhh/user/UserServiceUnitTest.java
+++ HealthyHeroesHub/src/test/java/hhh/user/UserServiceUnitTest.java
@@ -1,4 +1,35 @@
 package hhh.user;
+import hhh.user.model.User;
+import hhh.user.model.UserRole;
+import hhh.user.repository.UserRepository;
+import hhh.user.service.UserService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
 
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.security.crypto.password.PasswordEncoder;
+;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+
+@ExtendWith(MockitoExtension.class)
 public class UserServiceUnitTest {
+    @Mock
+    private UserRepository userRepository;
+
+    @Mock
+    private PasswordEncoder passwordEncoder;
+
+    private UserService userService;
+
+    @Test
+    public void when_createDefaultAdmin_thenCheckForAdminRoleAndUsernameAndPassword() {
+    }
 }
